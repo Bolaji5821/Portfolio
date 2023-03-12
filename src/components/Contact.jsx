3abb5debbf90e73ab98ac804294d22c1a9cb81b1
B_O_J <oyedelebolaji84@gmail.com>
2023-03-12 12:36:49 +0100

    hot fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 const Contact = () => {
+    const emailRef = useRef(null);
+
+    function copyEmailToClipboard() {
+        const email = emailRef.current.value;
+        navigator.clipboard.writeText(email);
+    }
+    const phoneRef = useRef(null);
+
+    function copyPhoneToClipboard() {
+        const phone = phoneRef.current.value;
+        navigator.clipboard.writeText(phone);
+    }
+
     return (
         <div name='contact' className='w-full h-screen bg-gradient-to-t from-[#0c0227] to-[#2b00ff] flex justify-center items-center p-4'>
             <div className='flex flex-col max-w-[600px] w-full'>
@@ -12,8 +25,15 @@ const Contact = () => {
                 <input className='my-4 p-2 bg-[#fff]' type="email" placeholder='Email' name='email' />
                 <textarea className='bg-[#fff] p-2' name="message" rows="10" placeholder='Message'></textarea>
                 <button className='text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+                <input className='text-black border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center' type="email" placeholder='Email' name='email' value="oyedelebolaji84@gmail.com" ref={emailRef} />
+                <input className='text-black border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center' type="phone" placeholder='Phone Number' name='phone' value="+2349025563043" ref={phoneRef} />
+                <button onClick={copyEmailToClipboard} className='text-black border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-1 mx-auto flex items-center'>Copy Email</button>
+                <button onClick={copyPhoneToClipboard} className='text-black border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-1 mx-auto flex items-center'>Copy Phone Number</button>
+
             </div>
+
         </div>
-    )
+    );
 }
-export default Contact
+
+export default Contact;
